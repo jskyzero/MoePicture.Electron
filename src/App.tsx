9dd20fce0b3b4087e746504fe2320ee66e3edfb8
jskyzero <jskyzero@outlook.com>
2018-12-18 17:22:44 +0800

    change to fabric start example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import * as React from 'react';
-import './App.css';
+// import * as React from 'react';
+// import './App.css';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+// class App extends React.Component {
+//   public render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <DefaultButton
+          text='See Button'
+          primary={true}
+          href='#/components/button'
+        />
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { initializeIcons } from '@uifabric/icons';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+// Register icons and pull the fonts from the default SharePoint CDN:
+initializeIcons();
 
 ReactDOM.render(
   <App />,
