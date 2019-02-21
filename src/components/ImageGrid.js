0026db281803b764b9c312171604e1ac9bf241bc
jskzyero <jskyzero@outlook.com>
2019-02-21 09:46:27 +0800

    back to previous version

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -32,10 +32,6 @@ class ImageGrid extends React.Component {
 
     return (
       <div>
-        <canvas>
-        {/* style={"background-url: https://files.yande.re/sample/09919d7ce26350f19b46bde905821908/yande.re%20516608%20sample%20chibi%20cleavage%20dress%20fate_grand_order%20heels%20jeanne_d%27arc%20jeanne_d%27arc_%28fate%29%20no_bra%20stockings%20symmetrical_docking%20thighhighs%20tonchan.jpg;" */}
-          {/* <div></div> */}
-        </canvas>
           {items}
       </div>
     )
